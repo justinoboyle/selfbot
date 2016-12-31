42f14178e33b47f534448fde64203051ef10364d
justinoboyle <justin@justinoboyle.com>
2016-12-31 00:03:23 -0500

    minor changes

--- index.js
+++ index.js
@@ -3,30 +3,38 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config');
 
+const fs = require('fs');
+
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const logFolder = path.join(__dirname, "logs");
+fs.mkdirSync(logFolder);
+
+app.static(logFolder);
+
+app.get('/', (req, res) =>
+    fs.listFiles(logFolder, files =>
+        res.send(
+            files.map(
+                e => `<a href='${e}.log'>${e}.log</a>`).join('<br />')
+        )
+    )
+)
+
 const bCorrector = {};
 const nogtext = {};
-
+const logger = require('./logger') || {};
 const bChar = "🅱️";
 
-global.embed =(url, title = url, description = 'ᅠ') => {
-  if (description !== 'ᅠ') description += '\n';
-  return {
-    title: title,
-    description: description,
-    url,
-    timestamp: new Date(),
-    video: { url },
-    image: { url },
-    footer: {
-      text: '',
-      icon_url: ''
-    }
-  }
-}
-
 client.on('message', msg => {
+    if (logger[msg.channel])
+        fs.appendFile(path.join(logFolder, `${msg.channel}.log`, `${Date.now()} ${msg.author.id}: ${msg.content}`));
+
     if (msg.author.id !== config.userid)
         return;
+
     if (msg.content.toLowerCase() == "/b on") {
         bCorrector[msg.channel] = true;
         return msg.delete();
@@ -36,6 +44,20 @@ client.on('message', msg => {
         bCorrector[msg.channel] = false;
         delete bCorrector[msg.channel];
         msg.delete();
+        return;
+    }
+
+    if (msg.content.toLowerCase() == "/log on") {
+        logger[msg.channel] = true;
+        saveLogger();
+        return msg.delete();
+    }
+
+    if (msg.content.toLowerCase() == "/log off") {
+        logger[msg.channel] = false;
+        delete logger[msg.channel];
+        msg.delete();
+        saveLogger();
         return;
     }
 
@@ -51,11 +73,11 @@ client.on('message', msg => {
         return;
     }
 
-    if(msg.content.startsWith('/eval ')) {
+    if (msg.content.startsWith('/eval ')) {
         try {
             eval(msg.content.substring('/eval '.length));
             msg.delete();
-        }catch(e) {
+        } catch (e) {
             msg.edit(e.toString());
         }
     }
@@ -71,12 +93,17 @@ client.on('message', msg => {
 
     if (!nogtext[msg.channel]) {
         setTimeout(() => {
-            if (msg.content.startsWith(">")) {
+            if (msg.content.startsWith(">"))
                 msg.edit("```css\n" + msg.content + "\n```")
-            }
         }, 100);
     }
 
 });
 
+function saveLogger() {
+    fs.writeFile('./logger.json', logger)
+}
+
 client.login(config.token);
+
+app.listen(config.port);
